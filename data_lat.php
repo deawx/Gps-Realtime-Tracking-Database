e0d329aa0176a815bfb1144f5be675f1117e2793
TFK-Project <taufik.developer@gmail.com>
2017-06-18 16:43:41 +0700

    Get latitude data from db
    Get latitude data from db

---
+++ data_lat.php
@@ -0,0 +1,9 @@
+<?php
+	//mysqli_connect("localhost","root","") or die ("Koneksi Gagal"); //koneksi
+	//mysqli_select_db("smartroro") or die ("Database tidak ditemukan");
+	include "koneksi.php";
+	$sql = mysqli_query($connection, "select * from tracking where id_gps = '1' "); //query untuk mendapatkan semua data mahasiswa
+	while ($data = mysqli_fetch_array($sql)){ // data akan di ulang
+		echo $data['lat'];
+	}
+?>
