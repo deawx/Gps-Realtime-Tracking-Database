3575f42ef2f58ef1ae7849f2dce6422646b3b8f8
TFK-Project <taufik.developer@gmail.com>
2017-06-18 16:44:18 +0700

    Add auto insert data
    Auto insert data for GET request

---
+++ insert_data_gps.php
@@ -0,0 +1,25 @@
+<?php
+include "koneksi.php";
+
+//tangkap parameter
+$id_gps		= "1";
+$latitude	= $_GET['lat'];
+$longitude	= $_GET['long'];
+$tanggal_waktu= 'CURRENT_TIMESTAMP';
+
+$sql	= "INSERT INTO `tracking`(`id_gps`, `lat`, `long`, `tanggal_waktu`) VALUES ('$id_gps', $latitude, $longitude, $tanggal_waktu)";
+//kalau mau pakai mysqli
+//$eksekusi = mysqli_query($koneksi, $sql);
+//kalau mau pakai mysql
+$eksekusi = mysqli_query($connection, $sql);
+
+if($eksekusi){
+	$sql2 = "UPDATE tracking SET `lat` = $latitude, `long` = $longitude WHERE id_gps = 1";
+	$hasil = mysqli_query($connection, $sql2) or die ("Maaf ada yang error".mysqli_error($connection));
+	echo "Data berhasil diupdate.";
+}else{
+	$sql3 = "UPDATE tracking SET `lat` = $latitude, `long` = $longitude WHERE id_gps = 1";
+	$hasil = mysqli_query($connection, $sql3) or die ("Maaf ada yang error".mysqli_error($connection));
+	echo "Data berhasil diupdate.";
+}
+?>
